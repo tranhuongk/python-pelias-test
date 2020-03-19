cdb1cbc572d839365b9e1421d2a24876bca6c215
danhquyen0109 <phamdanhquyen@gmail.com>
2020-03-19 21:09:55 +0700

    first commit

---
+++ code python test 44 22/all_even_housenumbers.py
@@ -0,0 +1,274 @@
+﻿
+data = [
+    {
+        "stt": "1",
+        "addr:housenumber": "2",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036550",
+        "lon": "105.789570"
+    },
+    {
+        "stt": "2",
+        "addr:housenumber": "4",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036550",
+        "lon": "105.789560"
+    },
+    {
+        "stt": "3",
+        "addr:housenumber": "6",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036540",
+        "lon": "105.789520"
+    },
+    {
+        "stt": "4",
+        "addr:housenumber": "8",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036520",
+        "lon": "105.789460"
+    },
+    {
+        "stt": "5",
+        "addr:housenumber": "10",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036530",
+        "lon": "105.789440"
+    },
+    {
+        "stt": "6",
+        "addr:housenumber": "12",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036540",
+        "lon": "105.789370"
+    },
+    {
+        "stt": "7",
+        "addr:housenumber": "14",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036550",
+        "lon": "105.789330"
+    },
+    {
+        "stt": "8",
+        "addr:housenumber": "16",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036570",
+        "lon": "105.789240"
+    },
+    {
+        "stt": "9",
+        "addr:housenumber": "18",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036550",
+        "lon": "105.789170"
+    },
+    {
+        "stt": "10",
+        "addr:housenumber": "36",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.788850"
+    },
+    {
+        "stt": "11",
+        "addr:housenumber": "38",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036600",
+        "lon": "105.788170"
+    },
+    {
+        "stt": "12",
+        "addr:housenumber": "40",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036620",
+        "lon": "105.788110"
+    },
+    {
+        "stt": "13",
+        "addr:housenumber": "42",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036620",
+        "lon": "105.788130"
+    },
+    {
+        "stt": "14",
+        "addr:housenumber": "44",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036620",
+        "lon": "105.788110"
+    },
+    {
+        "stt": "15",
+        "addr:housenumber": "46",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.788000"
+    },
+    {
+        "stt": "16",
+        "addr:housenumber": "48",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787980"
+    },
+    {
+        "stt": "17",
+        "addr:housenumber": "50",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787960"
+    },
+    {
+        "stt": "18",
+        "addr:housenumber": "52",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787940"
+    },
+    {
+        "stt": "19",
+        "addr:housenumber": "54",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787920"
+    },
+    {
+        "stt": "20",
+        "addr:housenumber": "56",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787900"
+    },
+    {
+        "stt": "21",
+        "addr:housenumber": "58",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787880"
+    },
+    {
+        "stt": "22",
+        "addr:housenumber": "60",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787860"
+    },
+    {
+        "stt": "23",
+        "addr:housenumber": "62",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787840"
+    },
+    {
+        "stt": "24",
+        "addr:housenumber": "64",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036650",
+        "lon": "105.787820"
+    },
+    {
+        "stt": "25",
+        "addr:housenumber": "66",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787780"
+    },
+    {
+        "stt": "26",
+        "addr:housenumber": "68",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787740"
+    },
+    {
+        "stt": "27",
+        "addr:housenumber": "70",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036630",
+        "lon": "105.787650"
+    },
+    {
+        "stt": "28",
+        "addr:housenumber": "72",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036660",
+        "lon": "105.787560"
+    },
+    {
+        "stt": "29",
+        "addr:housenumber": "120",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036710",
+        "lon": "105.786830"
+    },
+    {
+        "stt": "30",
+        "addr:housenumber": "124",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036780",
+        "lon": "105.786740"
+    },
+    {
+        "stt": "31",
+        "addr:housenumber": "130",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036840",
+        "lon": "105.785180"
+    },
+    {
+        "stt": "32",
+        "addr:housenumber": "136",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.037010",
+        "lon": "105.783340"
+    },
+    {
+        "stt": "33",
+        "addr:housenumber": "144",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036980",
+        "lon": "105.782090"
+    },
+    {
+        "addr:housenumber": "250",
+        "addr:street": "Xuân Thuỷ",
+        "type": "node",
+        "lat": "21.036980",
+        "lon": "105.782090"
+    }
+]
---
+++ code python test 44 22/test_noi_suy.py
@@ -0,0 +1,52 @@
+import json
+import urllib
+import requests
+import math
+import test_data_44_22
+import all_even_housenumbers
+
+def distance(lat1, lon1, lat2, lon2):
+    if (lat1 == lat2) and (lon1 == lon2):
+        return 0
+    else:
+        radlat1 = math.pi * lat1 / 180
+        radlat2 = math.pi * lat2 / 180
+        theta = lon1 - lon2
+        radtheta = math.pi * theta / 180
+        dist = math.sin(radlat1) * math.sin(radlat2) + math.cos(radlat1) * math.cos(radlat2) * math.cos(radtheta)
+        if dist > 1:
+            dist = 1
+        dist = math.acos(dist)
+        dist = dist * 180 / math.pi
+        dist = dist * 60 * 1.1515
+        return dist * 1.609344 * 1000
+        
+pass_count = 0
+total = 0
+
+# write distance into result file 
+with open("interpolation_result.js", "a") as result_file:
+    result_file.write('const interpolation_data = {"type":"FeatureInterpolation","features": [')
+    for each_test in test_data_44_22.test_datas:
+        total += 1
+        housenumber = each_test["housenumber"]
+        result = requests.get(f'http://localhost:3000/search/geojson?lat=21.036&lon=105.785&number={housenumber}&street=Xuan%20Thuy').json()
+        lat1 = result['properties']['lat']
+        lon1 = result['properties']['lon']
+        lat2 = float(each_test['real_lat'])
+        lon2 = float(each_test['real_lon'])
+        result_file.write(str(result)+", ")
+        #check pass test
+        devia = math.inf
+        if devia > distance(lat1, lon1, lat2, lon2):
+            devia = round(distance(lat1, lon1, lat2, lon2) * 100) / 100
+            if devia <= 20.0:
+                print("PASS")
+                pass_count += 1
+            else:
+                print("FAIL")
+    result_file.write(']}')
+    result_file.close()
+    
+print(pass_count)
+print(total)                
